f42ef2119cb06cbf056e0c9092296f5a8cb4acb4
LaurenRobertTodd <lt372@cornell.edu>
2013-10-01 20:10:04 -0700

    done

---
+++ lab2/slave_part3.c
@@ -0,0 +1,25 @@
+#include "mw.h"
+#include "mw_api.h"
+#include "def_structs_part3.h"
+
+void be_a_slave(struct mw_api_spec *f)
+{
+  mw_work_t work;
+  mw_result_t * computedResult;
+  MPI_Status status;
+
+  while(1)
+  {
+    MPI_Recv(&work, f->work_sz, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+	DEBUG_PRINT("Slave received work!");
+	if(status.MPI_TAG == KILL_TAG)
+	{
+		return;
+	}
+
+    computedResult = f->compute(&work);
+
+    MPI_Send(computedResult, f->res_sz, MPI_CHAR, 0, WORK_TAG, MPI_COMM_WORLD);
+	DEBUG_PRINT("Slave completed work!");
+  }
+}
