879b0e5694493e0b0e49b9948a65f06b6e80fa66
LaurenRobertTodd <lt372@cornell.edu>
2013-10-20 12:39:09 -0700

    added ping to slave and updated fail

--- lab3/slave.c
+++ lab3/slave.c
@@ -5,11 +5,13 @@
 #include <stdlib.h>
 #include <limits.h>
 
+// success probability
 static float p = 0.8;
 
 int F_Send(void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
 {
-  if (random_fail()) {      MPI_Finalize();
+  if (random_fail()) {      
+    MPI_Finalize();
     exit (0);
     return 0;
   } else {
@@ -20,9 +22,8 @@ int F_Send(void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_C
 // TODO: implement random_fail()
 bool random_fail()
 {
-  time_t t;
-  srand(time(&t));
-  float rand = ((float) rand())/UINT_MAX;
+  srand((unsigned)time(NULL));
+  float rand = ((float) rand())/RAND_MAX;
   return rand > p;
 }
 
@@ -30,10 +31,12 @@ void be_a_slave(int argc, char** argv, struct mw_api_spec *f)
 {
   mw_work_t work;
   mw_result_t * computedResult;
+  int ping = 1;
   MPI_Status status;
 
   while(1)
   {
+    // recv unit of work from master
     MPI_Recv(&work, f->work_sz, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 	if(status.MPI_TAG == KILL_TAG)
 	{
@@ -41,11 +44,13 @@ void be_a_slave(int argc, char** argv, struct mw_api_spec *f)
 	}
 
     computedResult = f->compute(&work);
-
-    // TODO: send ping after unit of work
-
+    
     DEBUG_PRINT("Sending result back!");
+    // send unit of work to master with probability p
     F_Send(computedResult, f->res_sz, MPI_CHAR, 0, WORK_TAG, MPI_COMM_WORLD);
     DEBUG_PRINT("result sent");
+
+    // TODO: send ping after unit of work is possibly sent
+    MPI_Send(&ping, 1, MPI_INT, 1, PING_TAG, MPI_COMM_WORLD);
   }
 }
