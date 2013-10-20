b1c1ca22d0bda69ff6ca426f3bb8c591247377d7
Robert Parks <robert@robert-Aspire-V3-772.(none)>
2013-10-20 12:04:34 -0400

    changed be_a_slave signature

--- lab3/mw.h
+++ lab3/mw.h
@@ -4,7 +4,7 @@
 #include "mw_api.h"
 
 void do_master_stuff(int argc, char ** argv, struct mw_api_spec *f);
-void be_a_slave(struct mw_api_spec *f);
+void be_a_slave(int argc, char ** argv, struct mw_api_spec *f);
 
 enum { WORK_TAG, KILL_TAG, PING_TAG };
 
