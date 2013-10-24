bbf69195aa52a7ef82893daf845b0993d1b8e940
LaurenRobertTodd <lt372@cornell.edu>
2013-10-24 10:06:04 -0700

    updating methodology

--- lab3/supervisor.c
+++ lab3/supervisor.c
@@ -3,11 +3,6 @@
 #include "mw.h"
 #include "def_structs.h"
 
-// sleeps
-
-// as loop, does non-blocking recv for other workers
-
-// sends failures to master
 
 void do_supervisor_stuff(int argc, char ** argv, struct mw_api_spec *f)
 {
@@ -18,76 +13,92 @@ void do_supervisor_stuff(int argc, char ** argv, struct mw_api_spec *f)
   MPI_Comm_size(MPI_COMM_WORLD, &number_of_slaves);
   number_of_slaves = number_of_slaves - 2;
   
-  MPI_Status status;
-  MPI_Request request;
+  MPI_Status status1, status2;
+  MPI_Request request1, request2;
   
-  // who has which unit of work
-  int * assignment_number = malloc(sizeof(int)*number_of_slaves);
   // keep track of start times
-  double * assignment_time = malloc(sizeof(double)*number_of_slaves);
+  double * assignment_time1 = malloc(sizeof(double)*number_of_slaves);
+  double * assignment_time2 = malloc(sizeof(double)*number_of_slaves);
+
   // determine how long each worker took
   double * complete_time = malloc(sizeof(double)*number_of_slaves);
+  // booleans for completion
   int * completed = calloc(sizeof(int), number_of_slaves);
+  int * failed_worker = calloc(sizeof(int), number_of_slaves);
 
   // supervisor does blocking receive to get list of workers and their start times
-  MPI_Recv(&assignment_number, number_of_slaves, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-  MPI_Recv(&assignment_time, number_of_slaves, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+  MPI_Recv(&assignment_time1, number_of_slaves, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
   
   // calc approximate time diff between sup and master
-  double master_time = assignment_time[number_of_slaves-1];
+  double master_time = assignment_time1[number_of_slaves-1];
   double mytime_off_by = MPI_Wtime() - master_time;
 
-  DEBUG_PRINT("supervisor knows what the workers are doing and when they started");
+  DEBUG_PRINT("supervisor knows when the workers started");
 
   int units_received = 0;
-  int msg = 0, flag=0;
+  int failed_worker;
+  int i;
+  int kill_msg, flag1, flag2;
   
-  MPI_IRecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, PING_TAG, MPI_COMM_WORLD, &request);
+  double tot_time=0, mean=0, stddev=0;
   
-  double tot_time;
-
-  // does non-blocking recv for pings from workers for first 50%
-  while(units_received < number_of_slaves/2) 
+  //waiting for updates on start times from master
+  while(1) 
   {
-    MPI_Test(&request, &flag, &status);
-    if(flag)
+    //kill myself if the master says so
+    MPI_IRecv(&kill_msg, 1, MPI_INT, 0, KILL_TAG, MPI_COMM_WORLD, &request1);
+    MPI_Test(&request1, &flag1, &status1);
+    if(flag1)
     {
-      int pid = status.MPI_SOURCE;
-      int slave = pid - 2;
+      exit(0);
+    }
+    
+    //get a new start time array from master
+    MPI_IRecv(&assignment_time2, 1, MPI_INT, 0, SUPERVISOR_TAG, MPI_COMM_WORLD, &request2);
+    MPI_Test(&request2, &flag2, &status2);
+    if(!flag2){
+      continue;
+    }
 
-      if(complete_time[slave] == NULL) 
+    //check for differences in working slaves
+    for(i=0; i<number_of_slaves; i++) 
+    {
+      if(failed_worker[i] == 0)
       {
-        //actual time it takes to complete, relative to when master sent
-        complete_time[slave] = MPI_Wtime() - mytime_off_by - assignment_time[slave];
-        completed[slave] = 1;
-        units_received++;
-        tot_time += complete_time[slave];
+        //we have a good worker!
+        if(assignment_time1[i] != assignment_time2[i])
+        {
+          completed[i] = 1;
+          units_received++;
+          complete_time[i] = assignment_time2[i] - assignment_time1[i];
+          tot_time += complete_time[i];
+          
+          //we have enough data to update mean and create stddev
+          if(units_received >= number_of_slaves/2)
+          {
+            mean = tot_time/units_received;
+            // only calc stddev once
+            if(stddev==0)
+            {
+              stddev = stdDev(complete_time);
+            }
+            threshold = mean + 2*stddev;
+          }
+          
+        }
+        //we have a bad worker :(
+        else if(threshold>0 && MPI_Wtime() - mytime_off_by - assignment_time1[i] > threshold)
+        {
+          MPI_Send(i, 1, MPI_INT, 0, FAIL_TAG, MPI_COMM_WORLD);
+          failed_worker[i] = 1;
+        }
+      
       }
-    } 
+      
+    }
     
   }
   
-  double start_to_wait = MPI_Wtime();
-
-  // upon first 50%, computes mean and std dev
-  double mean_time = tot_time/units_received;
-  double standard_dev = stdDev(complete_time);
-  
-  while(units_received<number_of_slaves && MPI_Wtime() <= start_to_wait + 3*standard_dev) 
-  {
-    MPI_Test(&request, &flag, &status);
-    if(flag)
-    {
-      pid = status.MPI_SOURCE;
-      completed[pid-2] = 1;
-      units_received++;
-    }
-  }
-  
-  MPI_Send(completed, number_of_slaves, MPI_INT, 0, FAIL_TAG, MPI_COMM_WORLD);
-  
-  MPI_IRecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, KILL_TAG, MPI_COMM_WORLD, &request);
-
   
 }
 
