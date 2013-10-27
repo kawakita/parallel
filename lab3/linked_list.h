660b0131be34d43464990391958c193114adcb31
Robert Parks <r1parks.ucsd@gmail.com>
2013-10-27 10:12:24 -0400

    hi :)

--- lab3/linked_list.h
+++ lab3/linked_list.h
@@ -12,4 +12,5 @@ struct LinkedList
 /* array must be null terminated */
 LinkedList * listFromArray(mw_work_t ** array);
 LinkedList * new_linkedlist_node();
-void move_node_to_end(LinkedList * node);
+void move_node_to_end(LinkedList *);
+int list_length(LinkedList *)
