6fac9231957581c154224cbe9bc0b676e55e01d8
Robert Parks <r1parks.ucsd@gmail.com>
2013-10-27 13:01:33 -0400

    added test file

---
+++ lab3/test_part1.c
@@ -0,0 +1,62 @@
+#include <math.h>
+#include <gmp.h>
+#include "mw_api.h"
+#include "def_structs.h"
+#include "debug.h"
+
+mw_work_t ** create_work(int argc, char ** arv)
+{
+  const int total_work = 10;
+  mw_work_t ** work_list = calloc(total_work, sizeof(mw_work_t*) + 1);
+  int i=0;
+  for(i=0; i<total_work; ++i)
+  {
+    work_list[i] = malloc(sizeof(mw_work_t));
+    work_list[i]->x = i;
+  }
+  return work_list;
+}
+
+int process_results(int sz, mw_result_t * res)
+{
+  DEBUG_PRINT(("processing results!"));
+  int i;
+  for(i=0; i<sz; ++i)
+  {
+    DEBUG_PRINT(("%f", res[i].k));
+    if(res[i].k == M_PI)
+      DEBUG_PRINT(("Found some pi!"));
+    else
+      DEBUG_PRINT(("No pi here :("));
+  }
+  return 0;
+}
+
+mw_result_t * do_work(mw_work_t * work)
+{
+  mw_result_t * result = malloc(sizeof(mw_result_t));
+  result->k = M_PI;
+  DEBUG_PRINT(("created result %f\n", M_PI));
+  return result;
+}
+
+int main (int argc, char **argv)
+{
+  DEBUG_PRINT(("starting :)"));
+  struct mw_api_spec f;
+
+  MPI_Init (&argc, &argv);
+
+  f.create = create_work;
+  f.result = process_results;
+  f.compute = do_work;
+  f.work_sz = sizeof(struct userdef_work_t);
+  f.res_sz = sizeof(struct userdef_result_t);
+
+  MW_Run (argc, argv, &f);
+
+  MPI_Finalize ();
+
+  return 0;
+
+}
