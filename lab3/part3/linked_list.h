25eb58817dd76f074bbc00fc961061d17f3aa915
Robert Parks <r1parks.ucsd@gmail.com>
2013-10-29 05:32:08 -0400

    created new directory to work on part3

---
+++ lab3/part3/linked_list.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#include "mw_api.h"
+#include "debug.h"
+
+typedef struct LinkedList
+{
+	struct LinkedList * next;
+	mw_work_t * data;
+}
+LinkedList;
+
+/* array must be null terminated */
+LinkedList * listFromArray(mw_work_t ** array);
+LinkedList * new_linkedlist_node();
+void move_node_to_end(LinkedList *);
+int list_length(LinkedList *);
